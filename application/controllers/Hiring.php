3f9eab4df09d073f42ea12b75a3be28d4fa1ff31
tbvjaos510 <tbvjaos510@naver.com>
2018-11-24 11:36:30 +0900

    [ADD] 데리다 DB 이름 수정

---
+++ application/controllers/Hiring.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Created by PhpStorm.
+ * User: 서서섯서핫사삿아상희흐히히히ㅣㅎ
+ * Date: 2018-11-24
+ * Time: 오전 11:06
+ */
+
+class Hiring extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
---
+++ application/models/Recruit_Model.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Created by PhpStorm.
+ * User: tbvja
+ * Date: 2018-11-24
+ * Time: 오전 11:07
+ */
+
+class Recruit_Model extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    function Add_Recruit() {
+
+    }
+}
