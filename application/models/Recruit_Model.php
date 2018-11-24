1e6f2bf62b293bdd539f246c1d460625bc64fa54
tbvjaos510 <tbvjaos510@naver.com>
2018-11-24 15:34:07 +0900

    [ADD] 채용 공고 모딜 추가

--- application/models/Recruit_Model.php
+++ application/models/Recruit_Model.php
@@ -14,7 +14,72 @@ class Recruit_Model extends CI_Model
         parent::__construct();
     }
 
-    function Add_Recruit() {
 
+    /**
+     * 채용 공고를 추가합니다.
+     * @param $co_idx number 회사 번호
+     * @param $re_startDate DateTime 시작 시간
+     * @param $re_endDate DateTime 종료 시간
+     * @param $re_content String 내용
+     * @return mixed SQL Value
+     */
+    function Insert_Recruit($co_idx, $re_startDate, $re_endDate, $re_content) {
+        $sql = "insert into RECRUIT_TB (co_idx, re_startDate, re_endDate, re_content) values (?, ?, ?, ?)";
+        $bind = array(
+           $co_idx,
+           $re_startDate,
+           $re_endDate,
+           $re_content
+        );
+
+        return $this->db->query($sql, $bind);
+    }
+
+    /**
+     * 채용 공고를 삭제합니다.
+     * @param $re_idx number 채용공고 번호
+     * @return mixed SQL Value
+     */
+    function Delete_Recruit($re_idx) {
+        $sql = "delete from RECRUIT_tB where re_idx = ?";
+        $bind = array(
+           $re_idx
+        );
+
+        return $this->db->query($sql, $bind);
+    }
+
+
+    /**
+     * 채용 공고를 변경합니다.
+     * @param $re_idx number 채용공고 번호
+     * @param $re_startDate DateTime 시작 시간 수정 값
+     * @param $re_endDate DateTime 종료 시간 수정 값
+     * @param $re_content String 종료 시간
+     * @return mixed SQL Value
+     */
+    function Change_Recruit($re_idx, $re_startDate, $re_endDate, $re_content) {
+        $sql = "update RECRUIT_TB set re_startDate = ?, re_endDate = ?, re_content = ? where re_idx = ?";
+        $bind = array(
+           $re_startDate,
+           $re_endDate,
+           $re_content,
+           $re_idx
+        );
+
+        return $this->db->query($sql, $bind);
+    }
+
+    /**
+     * 채용 공고의 리스트를 들고 옵니다.
+     * @param $co_idx number 회사 번호
+     * @return mixed SQL Value
+     */
+    function List_Recruit($co_idx) {
+        $sql = "select * from RECRUIT_TB where co_idx = ?";
+        $bind = array(
+            $co_idx
+        );
+        return $this->db->query($sql, $bind);
     }
 }
