4e9b4e949517e9d1d6e5cff503b327e018c3b4a4
김민기 <alsrltt@nate.com>
2018-12-08 15:00:30 +0900

    [ADD] 일반프로필, 기업프로필 추가(아까안들어감)

---
+++ application/models/ProfileModel.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KMG
+ * Date: 2018-12-08
+ * Time: 오후 12:16
+ */
+
+Class ProfileModel extends CI_Model{
+
+    function __construct() {
+        parent::__construct();
+    }
+
+}
+?>
