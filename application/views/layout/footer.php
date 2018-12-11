a526198749aa455d94f9010a996201f3cb22fbc2
남현욱 <37534735+hw0k@users.noreply.github.com>
2018-12-11 09:48:05 +0900

    [ADD] View layout 헤더/푸터 추가

---
+++ application/views/layout/footer.php
@@ -0,0 +1,41 @@
+<script src="<?= site_url('js/jquery-3.2.1.js') ?>"></script>
+<script src="<?= site_url('js/jquery.appear.js') ?>"></script>
+<script src="<?= site_url('js/jquery.mousewheel.js') ?>"></script>
+<script src="<?= site_url('js/perfect-scrollbar.js') ?>"></script>
+<script src="<?= site_url('js/jquery.matchHeight.js') ?>"></script>
+<script src="<?= site_url('js/svgxuse.js') ?>"></script>
+<script src="<?= site_url('js/imagesloaded.pkgd.js') ?>"></script>
+<script src="<?= site_url('js/Headroom.js') ?>"></script>
+<script src="<?= site_url('js/velocity.js') ?>"></script>
+<script src="<?= site_url('js/ScrollMagic.js') ?>"></script>
+<script src="<?= site_url('js/jquery.waypoints.js') ?>"></script>
+<script src="<?= site_url('js/jquery.countTo.js') ?>"></script>
+<script src="<?= site_url('js/popper.min.js') ?>"></script>
+<script src="<?= site_url('js/material.min.js') ?>"></script>
+<script src="<?= site_url('js/bootstrap-select.js') ?>"></script>
+<script src="<?= site_url('js/smooth-scroll.js') ?>"></script>
+<script src="<?= site_url('js/selectize.js') ?>"></script>
+<script src="<?= site_url('js/swiper.jquery.js') ?>"></script>
+<script src="<?= site_url('js/moment.js') ?>"></script>
+<script src="<?= site_url('js/daterangepicker.js') ?>"></script>
+<script src="<?= site_url('js/simplecalendar.js') ?>"></script>
+<script src="<?= site_url('js/fullcalendar.js') ?>"></script>
+<script src="<?= site_url('js/isotope.pkgd.js') ?>"></script>
+<script src="<?= site_url('js/ajax-pagination.js') ?>"></script>
+<script src="<?= site_url('js/Chart.js') ?>"></script>
+<script src="<?= site_url('js/chartjs-plugin-deferred.js') ?>"></script>
+<script src="<?= site_url('js/circle-progress.js') ?>"></script>
+<script src="<?= site_url('js/loader.js') ?>"></script>
+<script src="<?= site_url('js/run-chart.js') ?>"></script>
+<script src="<?= site_url('js/jquery.magnific-popup.js') ?>"></script>
+<script src="<?= site_url('js/jquery.gifplayer.js') ?>"></script>
+<script src="<?= site_url('js/mediaelement-and-player.js') ?>"></script>
+<script src="<?= site_url('js/mediaelement-playlist-plugin.min.js') ?>"></script>
+
+<script src="<?= site_url('js/base-init.js') ?>"></script>
+<script defer src="<?= site_url('fonts/fontawesome-all.js') ?>"></script>
+
+<script src="<?= site_url('Bootstrap/dist/js/bootstrap.bundle.js') ?>"></script>
+
+</body>
+</html>
---
+++ application/views/layout/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <script src="<?= site_url('js/webfontloader.min.js') ?>"></script>
+
+  <script>
+    Webfont.load({
+      google: {
+        families: ['Roboto:300,400,500,700:latin']
+      }
+    });
+  </script>
+
+  <link rel="stylesheet" href="<?= site_url('Bootstrap/dist/css/bootstrap-reboot.css') ?>">
+  <link rel="stylesheet" href="<?= site_url('Bootstrap/dist/css/bootstrap.css') ?>">
+  <link rel="stylesheet" href="<?= site_url('Bootstrap/dist/css/bootstrap-grid.css') ?>">
+  
+  <link rel="stylesheet" href="<?= site_url('css/main.min.css') ?>">
+  <link rel="stylesheet" href="<?= site_url('css/fonts.min.css') ?>">
+  <title>데리다</title>
+</head>
+<body>
