ac5e5e6e389fe1026df465ec3fc2b6f8ef78de5a
남현욱 <37534735+hw0k@users.noreply.github.com>
2018-12-11 14:14:03 +0900

    [ADD] Test View

---
+++ application/views/intro/test.php
@@ -0,0 +1,157 @@
+<div class="container">
+	<div class="row">
+		<div class="col col-xl-12">
+			<h2 class="presentation-margin">Top Headers</h2>
+    </div>
+    
+		<!-- Top Header-Profile -->
+		
+		<div class="container">
+			<div class="row">
+				<div class="col col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+					<div class="ui-block">
+						<div class="top-header">
+							<div class="top-header-thumb">
+								<img src="img/top-header1.jpg" alt="nature">
+							</div>
+							<div class="profile-section">
+								<div class="row">
+									<div class="col col-lg-5 col-md-5 col-sm-12 col-12">
+										<ul class="profile-menu">
+											<li>
+												<a href="02-ProfilePage.html" class="active">Timeline</a>
+											</li>
+											<li>
+												<a href="05-ProfilePage-About.html">About</a>
+											</li>
+											<li>
+												<a href="06-ProfilePage.html">Friends</a>
+											</li>
+										</ul>
+									</div>
+									<div class="col col-lg-5 ml-auto col-md-5 col-sm-12 col-12">
+										<ul class="profile-menu">
+											<li>
+												<a href="07-ProfilePage-Photos.html">Photos</a>
+											</li>
+											<li>
+												<a href="09-ProfilePage-Videos.html">Videos</a>
+											</li>
+											<li>
+												<div class="more">
+													<svg class="olymp-three-dots-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-three-dots-icon"></use></svg>
+													<ul class="more-dropdown more-with-triangle">
+														<li>
+															<a href="#">Report Profile</a>
+														</li>
+														<li>
+															<a href="#">Block Profile</a>
+														</li>
+													</ul>
+												</div>
+											</li>
+										</ul>
+									</div>
+								</div>
+								<div class="control-block-button">
+									<a href="35-YourAccount-FriendsRequests.html" class="btn btn-control bg-blue">
+										<svg class="olymp-happy-face-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-happy-face-icon"></use></svg>
+									</a>
+									<a href="#" class="btn btn-control bg-purple">
+										<svg class="olymp-chat---messages-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-chat---messages-icon"></use></svg>
+									</a>
+									<div class="btn btn-control bg-primary more">
+										<svg class="olymp-settings-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-settings-icon"></use></svg>
+										<ul class="more-dropdown more-with-triangle triangle-bottom-right">
+											<li>
+												<a href="#" data-toggle="modal" data-target="#update-header-photo">Update Profile Photo</a>
+											</li>
+											<li>
+												<a href="#" data-toggle="modal" data-target="#update-header-photo">Update Header Photo</a>
+											</li>
+											<li>
+												<a href="29-YourAccount-AccountSettings.html">Account Settings</a>
+											</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="top-header-author">
+								<a href="02-ProfilePage.html" class="author-thumb">
+									<img src="img/author-main1.jpg" alt="author">
+								</a>
+								<div class="author-content">
+									<a href="02-ProfilePage.html" class="h4 author-name">James Spiegel</a>
+									<div class="country">San Francisco, CA</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+    <!-- ... end Top Header-Profile -->
+    
+		<div class="container">
+			<div class="row">
+				<div class="col col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+					<div class="ui-block">
+						<div class="top-header top-header-favorit">
+							<div class="top-header-thumb">
+								<img src="img/top-header2.jpg" alt="nature">
+								<div class="top-header-author">
+									<div class="author-thumb">
+										<img src="img/author-main2.jpg" alt="author">
+									</div>
+									<div class="author-content">
+										<a href="#" class="h3 author-name">Green Goo Rock</a>
+										<div class="country">Rock Band  |  San Francisco, CA</div>
+									</div>
+								</div>
+							</div>
+							<div class="profile-section">
+								<div class="row">
+									<div class="col col-xl-8 m-auto col-lg-8 col-md-12">
+										<ul class="profile-menu">
+											<li>
+												<a href="12-FavouritePage.html" class="active">Timeline</a>
+											</li>
+											<li>
+												<a href="13-FavouritePage-About.html">About</a>
+											</li>
+											<li>
+												<a href="07-ProfilePage-Photos.html">Photos</a>
+											</li>
+											<li>
+												<a href="09-ProfilePage-Videos.html">Videos</a>
+											</li>
+											<li>
+												<a href="14-FavouritePage-Statistics.html">Statistics</a>
+											</li>
+											<li>
+												<a href="15-FavouritePage-Events.html">Events</a>
+											</li>
+										</ul>
+									</div>
+								</div>
+								<div class="control-block-button">
+									<a href="#" class="btn btn-control bg-primary">
+										<svg class="olymp-star-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-star-icon"></use></svg>
+									</a>
+									<a href="#" class="btn btn-control bg-purple">
+										<svg class="olymp-chat---messages-icon"><use xlink:href="svg-icons/sprites/icons.svg#olymp-chat---messages-icon"></use></svg>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<a class="back-to-top" href="#">
+	<img src="svg-icons/back-to-top.svg" alt="arrow" class="back-icon">
+</a>
--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-  <script src="<?= site_url('js/webfontloader.min.js') ?>"></script>
+  <script src="<?= site_url('assets/js/webfontloader.min.js') ?>"></script>
 
   <script>
     Webfont.load({
@@ -15,12 +15,12 @@
     });
   </script>
 
-  <link rel="stylesheet" href="<?= site_url('Bootstrap/dist/css/bootstrap-reboot.css') ?>">
-  <link rel="stylesheet" href="<?= site_url('Bootstrap/dist/css/bootstrap.css') ?>">
-  <link rel="stylesheet" href="<?= site_url('Bootstrap/dist/css/bootstrap-grid.css') ?>">
+  <link rel="stylesheet" href="<?= site_url('assets/Bootstrap/dist/css/bootstrap-reboot.css') ?>">
+  <link rel="stylesheet" href="<?= site_url('assets/Bootstrap/dist/css/bootstrap.css') ?>">
+  <link rel="stylesheet" href="<?= site_url('assets/Bootstrap/dist/css/bootstrap-grid.css') ?>">
   
-  <link rel="stylesheet" href="<?= site_url('css/main.min.css') ?>">
-  <link rel="stylesheet" href="<?= site_url('css/fonts.min.css') ?>">
+  <link rel="stylesheet" href="<?= site_url('assets/css/main.min.css') ?>">
+  <link rel="stylesheet" href="<?= site_url('assets/css/fonts.min.css') ?>">
   <title>데리다</title>
 </head>
 <body>
