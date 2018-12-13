8d8e9dfd18749c703675a4bb7f72c882e0e8dea6
wiber0205 <qownsgus0205@naver.com>
2018-12-13 15:16:59 +0900

    [UPDATE] 리스트 width, 마진 조절

--- application/views/hiring/hiringList.php
+++ application/views/hiring/hiringList.php
@@ -4,7 +4,7 @@
 
 <div class="container">
 	<div class="row">
-		<div class="col col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+		<div class="col col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top: 1rem">
 			<div class="ui-block responsive-flex1200">
 				<div class="ui-block-title">
 					<div class="w-select">
@@ -47,7 +47,7 @@
 	</div>
 </div>
 
-<section class="blog-post-wrap medium-padding80">
+<section class="blog-post-wrap medium-padding80" style="padding-top: 20px; padding-bottom: 0;">
 	<div class="container">
 		<div class="row">
 
@@ -55,7 +55,7 @@
                 foreach($hiringList as $item) {
 
             ?>
-            <div class="col col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
+            <div class="col col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
                 <div class="ui-block">
 
 
@@ -87,7 +87,7 @@
 <!--							<p>Here’s a photo from last month’s photoshoot. We got really awesome shots for the new catalog.</p>-->
 
 							<div class="author-date">
-								<a class="h6 post__author-name fn" href="/member/Company?me_c_idx=<?= $item['me_c_idx'] ?>"><?= $item['me_c_name'] ?></a>
+								<a class="h6 post__author-name fn" href="#"><?= $item['me_c_name'] ?></a>
 								<div class="post__date">
 									<span class="published">
                                         <?php
@@ -107,7 +107,7 @@
 							<div class="post-additional-info inline-items">
 					
 								<div class="comments-shared">
-									<a href="/hiring/detail?re_idx=<?= $item['re_idx']?>" class="post-add-icon inline-items">
+									<a href="#" class="post-add-icon inline-items">
 										<svg class="olymp-speech-balloon-icon"><use xlink:href="/assets/svg-icons/sprites/icons.svg#olymp-speech-balloon-icon"></use></svg>
 										<span><?= $item['recruitCnt'] ?>개의 분야 모집이 있습니다.</span>
 									</a>
@@ -130,7 +130,7 @@
 	<!-- Pagination -->
 	
 	<nav aria-label="Page navigation">
-		<ul class="pagination justify-content-center">
+		<ul class="pagination justify-content-center" style="margin-bottom: 35px; margin-top: 5px;">
 			<li class="page-item disabled">
 				<a class="page-link" href="#" tabindex="-1">Previous</a>
 			</li>
