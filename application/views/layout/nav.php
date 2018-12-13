a0ac4f9f776379f20d8233083d06675ac098a19c
남현욱 <37534735+hw0k@users.noreply.github.com>
2018-12-13 09:41:30 +0900

    [MODIFY] layout/nav UI 변경

--- application/views/layout/nav.php
+++ application/views/layout/nav.php
@@ -193,80 +193,6 @@
 					</a>
 				</div>
 			</div>
-<!--
-			<div class="ui-block-title ui-block-title-small">
-				<h6 class="title">MAIN SECTIONS</h6>
-			</div>
-
-			<ul class="left-menu">
-				<li>
-					<a href="#" class="js-sidebar-open">
-						<svg class="olymp-close-icon left-menu-icon"><use xlink:href="<?= site_url('assets/svg-icons/sprites/icons.svg#olymp-close-icon') ?>"></use></svg>
-						<span class="left-menu-title">Collapse Menu</span>
-					</a>
-				</li>
-				<li>
-					<a href="mobile-index.html">
-						<svg class="olymp-newsfeed-icon left-menu-icon" data-toggle="tooltip" data-placement="right"   data-original-title="NEWSFEED"><use xlink:href="<?= site_url('assets/svg-icons/sprites/icons.svg#olymp-newsfeed-icon') ?>"></use></svg>
-						<span class="left-menu-title">Newsfeed</span>
-					</a>
-				</li>
-				<li>
-					<a href="Mobile-28-YourAccount-PersonalInformation.html">
-						<svg class="olymp-star-icon left-menu-icon"  data-toggle="tooltip" data-placement="right"   data-original-title="FAV PAGE"><use xlink:href="<?= site_url('assets/svg-icons/sprites/icons.svg#olymp-star-icon') ?>"></use></svg>
-						<span class="left-menu-title">Fav Pages Feed</span>
-					</a>
-				</li>
-				<li>
-					<a href="mobile-29-YourAccount-AccountSettings.html">
-						<svg class="olymp-happy-faces-icon left-menu-icon"  data-toggle="tooltip" data-placement="right"   data-original-title="FRIEND GROUPS"><use xlink:href="<?= site_url('assets/svg-icons/sprites/icons.svg#olymp-happy-faces-icon') ?>"></use></svg>
-						<span class="left-menu-title">Friend Groups</span>
-					</a>
-				</li>
-				<li>
-					<a href="Mobile-30-YourAccount-ChangePassword.html">
-						<svg class="olymp-headphones-icon left-menu-icon"  data-toggle="tooltip" data-placement="right"   data-original-title="MUSIC&PLAYLISTS"><use xlink:href="<?= site_url('assets/svg-icons/sprites/icons.svg#olymp-headphones-icon') ?>"></use></svg>
-						<span class="left-menu-title">Music & Playlists</span>
-					</a>
-				</li>
-				<li>
-					<a href="Mobile-31-YourAccount-HobbiesAndInterests.html">
-						<svg class="olymp-weather-icon left-menu-icon"  data-toggle="tooltip" data-placement="right"   data-original-title="WEATHER APP"><use xlink:href="<?= site_url('assets/svg-icons/sprites/icons.svg#olymp-weather-icon') ?>"></use></svg>
-						<span class="left-menu-title">Weather App</span>
-					</a>
-				</li>
-				<li>
-					<a href="Mobile-32-YourAccount-EducationAndEmployement.html">
-						<svg class="olymp-calendar-icon left-menu-icon"  data-toggle="tooltip" data-placement="right"   data-original-title="CALENDAR AND EVENTS"><use xlink:href="<?= site_url('assets/svg-icons/sprites/icons.svg#olymp-calendar-icon') ?>"></use></svg>
-						<span class="left-menu-title">Calendar and Events</span>
-					</a>
-				</li>
-				<li>
-					<a href="Mobile-33-YourAccount-Notifications.html">
-						<svg class="olymp-badge-icon left-menu-icon"  data-toggle="tooltip" data-placement="right"   data-original-title="Community Badges"><use xlink:href="<?= site_url('assets/svg-icons/sprites/icons.svg#olymp-badge-icon') ?>"></use></svg>
-						<span class="left-menu-title">Community Badges</span>
-					</a>
-				</li>
-				<li>
-					<a href="Mobile-34-YourAccount-ChatMessages.html">
-						<svg class="olymp-cupcake-icon left-menu-icon"  data-toggle="tooltip" data-placement="right"   data-original-title="Friends Birthdays"><use xlink:href="<?= site_url('assets/svg-icons/sprites/icons.svg#olymp-cupcake-icon') ?>"></use></svg>
-						<span class="left-menu-title">Friends Birthdays</span>
-					</a>
-				</li>
-				<li>
-					<a href="Mobile-35-YourAccount-FriendsRequests.html">
-						<svg class="olymp-stats-icon left-menu-icon"  data-toggle="tooltip" data-placement="right"   data-original-title="Account Stats"><use xlink:href="<?= site_url('assets/svg-icons/sprites/icons.svg#olymp-stats-icon') ?>"></use></svg>
-						<span class="left-menu-title">Account Stats</span>
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<svg class="olymp-manage-widgets-icon left-menu-icon"  data-toggle="tooltip" data-placement="right"   data-original-title="Manage Widgets"><use xlink:href="<?= site_url('assets/svg-icons/sprites/icons.svg#olymp-manage-widgets-icon') ?>"></use></svg>
-						<span class="left-menu-title">Manage Widgets</span>
-					</a>
-				</li>
-			</ul>
-		-->
 			<div class="ui-block-title ui-block-title-small">
 				<h6 class="title">YOUR ACCOUNT</h6>
 			</div>
@@ -387,5 +313,28 @@
 	</div>
 
 </header>
+
+<div class="fixed-sidebar right">
+	<div class="fixed-sidebar-right sidebar--small" id="sidebar-right">
+		<div class="mCustomScrollbar ps ps--theme_default" data-mcs-theme="dark" data-ps-id="4b473b17-ee6e-a505-e9ef-7a62401be867">
+		</div>
+
+	</div>
+
+	<div class="fixed-sidebar-right sidebar--large" id="sidebar-right-1">
+
+		<div class="mCustomScrollbar ps ps--theme_default ps--active-y" data-mcs-theme="dark" data-ps-id="70482f89-fc87-f2d0-b47b-8872730dc778">
+		</div>
+
+	</div>
+</div>
+
+<div class="fixed-sidebar right fixed-sidebar-responsive">
+
+	<div class="fixed-sidebar-right sidebar--small" id="sidebar-right-responsive">
+
+	</div>
+
+</div>
 
 <div class="header-spacer-small"></div>
