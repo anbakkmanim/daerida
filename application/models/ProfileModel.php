3383417c5271b8440dd631dd615678e011e50f3e
LAPTOP-SBQOT7CR\Jin <womin1120@naver.com>
2018-12-13 16:40:36 +0900

    [MODIFY] 조인 구문 추가

--- application/models/ProfileModel.php
+++ application/models/ProfileModel.php
@@ -304,6 +304,7 @@ Class ProfileModel extends CI_Model{
         $sql = "SELECT *
                 FROM COMPANY_QNA_TB
                 WHERE me_c_idx = ?
+                JOIN MEMBER_NORMAL_TB on me_n_idx = me_n_idx
         ";
 
         $result = $this->db->query($sql, array($param['me_c_idx']));
