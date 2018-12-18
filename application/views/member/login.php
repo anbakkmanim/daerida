c9e159d1895d7aa3a705a71f36bd9f3f6ea1f039
남현욱 <37534735+hw0k@users.noreply.github.com>
2018-12-18 19:02:31 +0900

    [ADD] member/login 수정사항 적용 , layout/inner_header 수정사항 적용

--- application/views/member/login.php
+++ application/views/member/login.php
@@ -7,15 +7,34 @@
 <div class="header--standard header--standard-landing" id="header--standard">
 	<div class="container">
 		<div class="header--standard-wrap">
-			<a href="#" class="logo">
+			<a href="/" class="logo">
 				<div class="img-wrap">
-				<img src="<?= '/assets/img/데리다57_흰.png' ?>" alt="Olympus">
+					<img src="<?= '/assets/img/데리다57_흰.png' ?>" alt="Olympus">
+					<img src="<?= '/assets/img/데리다57_주.png' ?>" alt="Olympus" class="logo-colored">
 				</div>
 				<div class="title-block">
-					<h6 class="logo-title">데리다</h6>
-					<div class="sub-title">Daerida</div>
+					<h6 class="logo-title">DAERIDA</h6>
+					<div class="sub-title">전국 잡 매칭 서비스</div>
 				</div>
 			</a>
+
+			<div class="nav nav-pills nav1 header-menu">
+				<div class="mCustomScrollbar ps ps--theme_default ps--active-y" data-ps-id="fb9077b8-3b61-5ba7-d178-f024b0b54a3c">
+					<ul>
+						<li class="nav-item" style="opacity: 1; visibility: visible;">
+							<a href="/subpage/about">소개</a>
+						</li>
+						<li class="nav-item" style="opacity: 1; visibility: visible;">
+							<a href="/subpage/teamterview">팀터뷰</a>
+						</li>
+						<li class="nav-item" style="opacity: 1; visibility: visible;">
+							<a href="/subpage/support">지원</a>
+						</li>
+					</ul>
+					<div class="ps__scrollbar-x-rail" style="left: 0px; bottom: 0px;"><div class="ps__scrollbar-x" tabindex="0" style="left: 0px; width: 0px;"></div></div>
+					<div class="ps__scrollbar-y-rail" style="top: 0px; height: 73px; right: 0px;"><div class="ps__scrollbar-y" tabindex="0" style="top: 0px; height: 30px;"></div></div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
