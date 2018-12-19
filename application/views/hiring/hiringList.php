ad6646fa384d21cd42b55289ce4d1f6a53f783a1
tbvjaos510 <tbvjaos510@naver.com>
2018-12-19 23:29:58 +0900

    [MODIFY] Page Navigation 1만 넣음

--- application/views/hiring/hiringList.php
+++ application/views/hiring/hiringList.php
@@ -152,10 +152,6 @@ if ($mode == "all") {
 				<a class="page-link" href="#" tabindex="-1">Previous</a>
 			</li>
 			<li class="page-item"><a class="page-link" href="#">1<div class="ripple-container"><div class="ripple ripple-on ripple-out" style="left: -10.3833px; top: -16.8333px; background-color: rgb(255, 255, 255); transform: scale(16.7857);"></div></div></a></li>
-			<li class="page-item"><a class="page-link" href="#">2</a></li>
-			<li class="page-item"><a class="page-link" href="#">3</a></li>
-			<li class="page-item"><a class="page-link" href="#">...</a></li>
-			<li class="page-item"><a class="page-link" href="#">12</a></li>
 			<li class="page-item">
 				<a class="page-link" href="#">Next</a>
 			</li>
